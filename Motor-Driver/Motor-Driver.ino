231771da58c64ddfd68c5e21636383e907a38638
Rahul Shah <rshahshah2890@gmail.com>
2017-12-09 13:25:12 +0530

    Update dthe sketch

--- Motor-Driver/Motor-Driver.ino
+++ Motor-Driver/Motor-Driver.ino
@@ -8,13 +8,13 @@ volatile uint16_t ticks, ticks1, ticks2;
 
 ISR(TIMER1_CAPT_vect) {
 
-  if (subb == 0) {
+  if (digitalRead(8)==HIGH) {
     ticks1 = ICR1;
-    TCCR1B |= (subb << ICES1);
+    TCCR1B |= (0 << ICES1);
     subb = 1;
   } else {
     ticks2 = ICR1;
-    TCCR1B |= (subb << ICES1);
+    TCCR1B |= (1 << ICES1);
     subb = 0;
   }
   ticks = ticks2 - ticks1;
@@ -22,9 +22,9 @@ ISR(TIMER1_CAPT_vect) {
 void setup() {
   //pinmodes
   pinMode(8, INPUT);
-  pinMode(PD5, OUTPUT);
-  pinMode(PD6, OUTPUT);
-  Serial.begin(9600);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  
   //Timer1 setup
   TCCR1A = 0b00000000;
   TCCR1B = 0b01000011;
@@ -38,18 +38,15 @@ void setup() {
 }
 
 void loop() {
-  uint16_t ol;
-  if (ticks != ol) {
-    Serial.println(ticks);
-
+  ticks=ticks-136;
     if (ticks == 0) {
-      digitalWrite(5, LOW);
-      digitalWrite(6, LOW);
-    } else if (ticks > 0 && ticks <= 250) {
+      OCR0A=0;
+      OCR0B=0;
+    } else if (ticks > 0 && ticks <= 232) {
       OCR0B = ticks;
       OCR0A = 0;
-    } else if (ticks > 250 && ticks < 500) {
-      OCR0A = ticks - 250;
+    } else if (ticks > 232 && ticks < 464) {
+      OCR0A = ticks - 232;
       OCR0B = 0;
     }
   }
